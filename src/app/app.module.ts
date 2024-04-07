6c3e35d03cff6948146618fe8b7f93603a91515b
dike <h220278f@hit.ac.zw>
2024-04-07 16:51:41 +0200

    Implemented Basic crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { ItemComponent } from './item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,114 @@ import { Component } from '@angular/core';
 })
 export class ItemComponent {
 
-}
+    itemArray : any[] = [];
+    isResultLoaded = false;
+  
+   
+    
+    iName: string ="";
+    iDescription: string ="";
+    iQuantity: Number =0;
+   
+    itemID = "";
+  
+  
+  constructor(private http: HttpClient )
+    {
+      this.getAllitem();
+   
+    }
+  
+    getAllitem()
+    {
+      
+      this.http.get("http://127.0.0.1:8000/api/inventory/search")
+    
+      .subscribe((resultData: any)=>
+      {
+          this.isResultLoaded = true;
+          console.log(resultData);
+          this.itemArray = resultData;
+      });
+    }
+   
+    add()
+    {
+    
+      let bodyData = {
+        "iName" : this.iName,
+        "iDescription" : this.iDescription,
+        "iQuantity" : this.iQuantity,
+        "itemID":null
+      };
+   
+      this.http.post("http://127.0.0.1:8000/api/inventory/add",bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
+      {
+          console.log(resultData);
+          alert("Item added Successfully");
+          this.getAllitem();
+          this.iName = '';
+          this.iDescription = '';
+          this.iQuantity  = 0;
+      });
+    }
+    setUpdate(data: any)
+    {
+     this.iName = data.iName;
+     this.iDescription = data.iDescription;
+     this.iQuantity = data.iQuantity;
+     this.itemID = data.itemID;
+    }
+   
+    UpdateRecords()
+    {
+      let bodyData = {
+        "itemID" : this.itemID,
+        "iName" : this.iName,
+        "iDescription" : this.iDescription,
+        "iQuantity" : this.iQuantity,
+    
+      };
+      
+      this.http.put("http://127.0.0.1:8000/api/inventory/update",bodyData).subscribe((resultData: any)=>
+      {
+          console.log(resultData);
+          alert("Item updated ")
+          this.getAllitem();
+        
+      });
+    }
+   
+    save()
+    {
+      if(this.itemID == '')
+      {
+          this.add();
+      }
+        else
+        {
+         this.UpdateRecords();
+        }      
+   
+    }
+   
+    setDelete(data: any)
+    {
+      
+      
+      this.http.delete("http://127.0.0.1:8000/api/inventory/delete/itemID/"+ data.itemID,{responseType: 'text'}).subscribe((resultData: any)=>
+      {
+          console.log(resultData);
+          alert("Item deleted")
+          this.getAllitem();
+          this.iName = '';
+          this.iDescription = '';
+          this.iQuantity  = 0;
+    
+      });
+   
+    }
+  
+  
+  
+  }
